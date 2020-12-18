95c6cc3eb38ad07ec9e6cc7291ed5bc62415be29
RomanChet <67514938+RomanChet@users.noreply.github.com>
2020-12-18 16:15:20 +0300

    small edit of the code

--- app/src/main/java/com/example/weatherappinventos/apiprocessing/WeatherApiClient.kt
+++ app/src/main/java/com/example/weatherappinventos/apiprocessing/WeatherApiClient.kt
@@ -1,17 +1,27 @@
 package com.example.weatherappinventos.apiprocessing
 
+import android.app.Application
+import android.widget.Toast
+import com.example.weatherappinventos.MainActivity
+import com.example.weatherappinventos.SecondActivity
 import com.example.weatherappinventos.dataclass.CurrentDataWeather
 import com.example.weatherappinventos.dataclass.ForecastDataWeather
 import retrofit2.HttpException
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.system.exitProcess
 
 
 class WeatherApiClient {
 
-    private var apiKey = "cef1ebe434addacc0ea0911feea6b570" // account: Dev57
-    private val apiKeySecond = "8eb64f08f572d28625a4f9e180a4e369" // account: Dev577
-    private val apiKeyThird = "19c2555631ff13e224f58e3fe06f4a86" // account: Dev5777
+    private val apiKeys = arrayOf(
+        "cef1ebe434addacc0ea0911feea6b570",
+        "8eb64f08f572d28625a4f9e180a4e369",
+        "19c2555631ff13e224f58e3fe06f4a86",
+        "c783a94e8bde42f74c5c6c25a31fdd51"
+    )
+
+    private var apiKey = apiKeys[0]
 
     private val api = Retrofit.Builder()
         .baseUrl("https://api.openweathermap.org/data/2.5/")
@@ -33,22 +43,21 @@ class WeatherApiClient {
     suspend fun weatherForecast(
         cityName: String
     ): ForecastDataWeather {
-        return api.forecastWeatherCall(cityName, "metric", "ru", apiKey)
+        return try {
+            api.forecastWeatherCall(cityName, "metric", "ru", apiKey)
+        } catch (e: HttpException) {
+            changeApiKey(e.code())
+            api.forecastWeatherCall(cityName, "metric", "ru", apiKey)
+        }
     }
 
     // смена ключа API при достижении лимита запросов
     private fun changeApiKey(code: Int) {
-        while (code == 429) {
-            if (apiKey == "cef1ebe434addacc0ea0911feea6b570") {
-                apiKey = apiKeySecond
-                break
-            }
-            if (apiKey == apiKeySecond) {
-                apiKey = apiKeyThird
-                break
-            } else {
-                break
-            }
-        }
+        val sizeApiKeys = apiKeys.size
+        var index = apiKeys.indexOf(apiKey)
+        if (index + 1 <= sizeApiKeys && code == 429) {
+            index++
+            apiKey = apiKeys[index]
+        } else index = 0
     }
 }
